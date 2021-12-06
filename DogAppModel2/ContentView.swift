24125cca91ff5326b81107e63b6368bacaa7d2c2
NatPuff <cesarp102104@gmail.com>
2021-12-06 10:25:04 -0600

    Initial Commit

---
+++ DogAppModel2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DogAppModel2
+//
+//  Created by Cesar Pacheco on 12/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DogAppModel2/DogAppModel2App.swift
@@ -0,0 +1,17 @@
+//
+//  DogAppModel2App.swift
+//  DogAppModel2
+//
+//  Created by Cesar Pacheco on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct DogAppModel2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
